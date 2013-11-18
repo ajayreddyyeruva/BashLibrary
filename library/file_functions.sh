3dd23479b910497e92a11e8f15b275d08f112315
sandy <sandeep_r80@yahoo.co.in>
2013-11-18 10:20:18 +0530

    Added first version of file function

---
+++ library/file_functions.sh
@@ -0,0 +1,57 @@
+function createEmptyFile() {
+	>| "$1"	
+}
+
+# To search *log* FILE_REGEX=.*\(log\)*
+# To search *.log FILE_REGEX=.*.log 
+function findFilesOfDirectoryRecursively() {
+	local DIRECTORY_PATH_TO_SEARCH="$1"
+	local FILE_REGEX="$2"
+	local OUTPUT_FILE_NAME="$3"
+	find "$DIRECTORY_PATH_TO_SEARCH" -regex "$FILE_REGEX" -type f -follow > "$OUTPUT_FILE_NAME"
+}
+
+#Returns top N lines after sorting the content of file in Descending order
+function findNSortedLines() {
+	local INPUT_FILE="$1"
+	local OUTPUT_FILE="$2"
+	local NUMBER_OF_FILES="$3"
+	cat "$INPUT_FILE" | sort -gr | head -"$NUMBER_OF_FILES" > "$OUTPUT_FILE" 
+}
+
+#Returns 2 dimensional data first column is the size of file and second column is name of file
+function getSizeOfFiles() {
+	local INPUT_FILE="$1"
+	local OUTPUT_FILE="$2"
+	createEmptyFile ${OUTPUT_FILE}	
+	echo "Calculating of size of files in ${INPUT_FILE} to ${OUTPUT_FILE}"
+	while read fileName
+	do
+		du -sb "$fileName" >> "$OUTPUT_FILE"
+	done < "$INPUT_FILE"
+}
+
+#Returns 2 dimension data first column is the size of file and second column is name of file, with the filter of minimum size
+function findFilesAboveThresholdSize() {
+	local INPUT_FILE="$1"
+        local OUTPUT_FILE="$2"
+        local THRESHOLD_FILE_SIZE="$3"
+
+	getSizeOfFiles ${INPUT_FILE} ${INPUT_FILE}.tmp
+	createEmptyFile ${OUTPUT_FILE}
+	while read fileNameWithSize
+	do
+		echo "Processing file ${fileNameWithSize}"
+		sizeOfFile=`echo "$fileNameWithSize" | awk '{print $1}'`
+		if [ "${sizeOfFile}" -ge "${THRESHOLD_FILE_SIZE}" ]
+		then
+			echo "$fileNameWithSize" >> "$OUTPUT_FILE"
+		#else
+		#	exit 0
+		fi
+	done < "${INPUT_FILE}.tmp"
+}
+
+
+
+	
