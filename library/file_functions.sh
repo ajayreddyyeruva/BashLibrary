62cbcc53417385b2ee97099f294bb0264dfcdb30
sandy <sandeep_r80@yahoo.co.in>
2014-04-28 12:27:37 +0530

    Added a logging message

--- library/file_functions.sh
+++ library/file_functions.sh
@@ -221,6 +221,7 @@ function deleteOlderDirectories() {
 	PARENT_DIR=$1
 	DIRS_COUNT_TO_BE_SKIPPED=$2
 
+	echo "Removing directories under ${PARENT_DIR}"
 	cd ${PARENT_DIR}
 	DIR_COUNT=`ls -lrtd */ | wc -l`
 	ls -lrtd */ | head -$((${DIR_COUNT}-${DIRS_COUNT_TO_BE_SKIPPED})) | awk '{print $9}'
